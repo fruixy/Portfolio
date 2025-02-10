fb56d45110f4ab9602e8da827a580b39e969f35c
fruixy <93591168+fruixy@users.noreply.github.com>
2025-02-10 02:25:19 +0100

    responsive V2

--- site/assets/js/menu.js
+++ site/assets/js/menu.js
@@ -1,10 +1,21 @@
-const logo = document.getElementById("logo");
-const navMenu = document.getElementById("navmenu");
+const burgerMenu = document.getElementById("burger-menu");
+const navMenu = document.getElementById("nav-menu");
+
+
+burgerMenu.addEventListener("click", () => {
+    navMenu.classList.toggle("active"); 
+    burgerMenu.classList.toggle("active"); 
+});
+
+const experiences = document.querySelectorAll('.experience');
+
+experiences.forEach((experience) => {
     
-if (logo && navMenu) {
-    logo.addEventListener("click", () => {
-        navMenu.classList.toggle("active");
+    experience.addEventListener('click', () => {
+    
+        const description = experience.querySelector('.description');
+        if (description) {
+            description.classList.toggle('active'); 
+        }
     });
-} else {
-    console.error("Logo or nav-menu not found");
-}
+});
